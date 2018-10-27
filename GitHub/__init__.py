03870a79ed954b46fb83ab3f654c0a7855df1fd7
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-10-27 22:29:34 +0900

    refactor: Create getRepositories function

--- GitHub/__init__.py
+++ GitHub/__init__.py
@@ -17,6 +17,12 @@ iconPath = "%s/%s.svg" % (os.path.dirname(__file__), __name__)
 baseUrl = "https://api.github.com"
 
 
+def getRepositories():
+    req = request.Request(baseUrl + "/users/tsub/repos?per_page=20")
+    with request.urlopen(req) as res:
+        return json.loads(res.read().decode())
+
+
 def handleQuery(query):
     if query.isTriggered:
         stripped = query.string.strip()
@@ -29,26 +35,24 @@ def handleQuery(query):
         else:
             results = []
 
-            req = request.Request(baseUrl + "/users/tsub/repos?per_page=20")
-            with request.urlopen(req) as res:
-                data = json.loads(res.read().decode())
+            data = getRepositories()
 
-                for repo in data:
-                    name = repo['full_name']
-                    description = repo['description'] or ""
-                    url = repo['html_url']
+            for repo in data:
+                name = repo['full_name']
+                description = repo['description'] or ""
+                url = repo['html_url']
 
-                    results.append(Item(id=__prettyname__,
-                                        icon=iconPath,
-                                        text=name,
-                                        subtext=description,
-                                        actions=[
-                                           UrlAction("Open repository", url)
-                                        ]))
+                results.append(Item(id=__prettyname__,
+                                    icon=iconPath,
+                                    text=name,
+                                    subtext=description,
+                                    actions=[
+                                       UrlAction("Open repository", url)
+                                    ]))
 
-                if results:
-                    return results
+            if results:
+                return results
 
-                return Item(id=__prettyname__,
-                            icon=iconPath,
-                            text="No results.")
+            return Item(id=__prettyname__,
+                        icon=iconPath,
+                        text="No results.")
